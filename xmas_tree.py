8dd6a9b29a2354adaf9a041f3f913abe01601b05
PixelNoob <jesuschitty7@hotmail.com>
2015-12-09 22:59:31 -0300

    Create xmas_tree.py

---
+++ xmas_tree.py
@@ -0,0 +1,21 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+X = [0, 100, 0] # Green
+O = [0, 0, 0] # Black
+W = [255, 255, 255]  # White
+B = [139, 69, 19] # Brown
+
+xmas_tree= [
+O, O, O, X, X, O, O, O,
+O, O, X, X, X, X, O, O,
+O, X, X, X, X, X, X, O,
+O, O, X, X, X, X, O, O,
+O, X, X, X, X, X, X, O,
+X, X, X, X, X, X, X, X,
+O, O, O, B, B, O, O, O,
+O, O, O, B, B, O, O, O
+]
+
+sense.set_pixels(xmas_tree)
