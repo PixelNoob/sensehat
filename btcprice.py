304fae512954b2b7deb67b61328f92a1c0c25a67
PixelNoob <jesuschitty7@hotmail.com>
2016-08-13 18:41:14 -0300

    except added
    except added so the program doesnt crash when there is no internet.

--- btcprice.py
+++ btcprice.py
@@ -1,7 +1,10 @@
 from sense_hat import SenseHat
 import ystockquote
 sense = SenseHat()
-while true:  
-    tickerSymbol = 'BTCUSD=X'
-    allInfo = ystockquote.get_all(tickerSymbol)
-    sense.show_message("BTC Price = " + allInfo["price"], text_colour=[255, 255, 255], back_colour=[255, 215, 0])
+while true:
+    try:
+        tickerSymbol = 'BTCUSD=X'
+        allInfo = ystockquote.get_all(tickerSymbol)
+        sense.show_message("BTC Price = " + allInfo["price"], text_colour=[255, 255, 255], back_colour=[255, 215, 0])
+   except:
+       sense.show_message("unable to get price feed")
