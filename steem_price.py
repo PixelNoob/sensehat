6eeb0c6939e8515becb7aa7861fefa1cb89aacc0
PixelNoob <jesuschitty7@hotmail.com>
2016-08-17 15:46:46 -0300

    time added
    improved feed, waiting time added to avoid multiple api calls

--- steem_price.py
+++ steem_price.py
@@ -2,19 +2,19 @@ from sense_hat import SenseHat
 sense = SenseHat()
 from urllib.request import urlopen
 import json
-
-url = urlopen('https://poloniex.com/public?command=returnTicker').read()
-result = json.loads(url)
-
-SBD = result['BTC_SBD']['last']
-BTC = result['USDT_BTC']['last']
-Steem = result['BTC_STEEM']['last']
-SBD_USD = float(SBD) * float(BTC)
+import time
 
 while True:
   try:
-    sense.show_message("BTC " + BTC, text_colour=[255, 215, 0])
+    url = urlopen('https://poloniex.com/public?command=returnTicker').read()
+    result = json.loads(url)
+    SBD = result['BTC_SBD']['last']
+    BTC = result['USDT_BTC']['last']
+    Steem = result['BTC_STEEM']['last']
+    SBD_USD = float(SBD) * float(BTC)
+    sense.show_message("BTC " + '{0:.2f}'.format(BTC), text_colour=[255, 215, 0])
     sense.show_message("Steem " + Steem, text_colour=[7, 24, 138])
-    sense.show_message("SBD " + '{0:f}'.format(SBD_USD), text_colour=[4, 112, 22])
+    sense.show_message("SBD " + '{0:.2f}'.format(SBD_USD), text_colour=[4, 112, 22])
+    time.sleep(3)
   except:
     sense.show_message("failed to retrieve feed")
