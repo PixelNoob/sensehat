e43dbb8258ccca0a37237523694f01fcc0494b2f
PixelNoob <jesuschitty7@hotmail.com>
2016-08-17 12:49:56 -0300

    try & except
    error handling

--- steem_price.py
+++ steem_price.py
@@ -11,6 +11,10 @@ BTC = result['USDT_BTC']['last']
 Steem = result['BTC_STEEM']['last']
 SBD_USD = float(SBD) * float(BTC)
 
-sense.show_message("BTC " + BTC, text_colour=[255, 215, 0])
-sense.show_message("Steem " + Steem, text_colour=[7, 24, 138])
-sense.show_message("SBD " + '{0:f}'.format(SBD_USD), text_colour=[4, 112, 22])
+while True:
+  try:
+    sense.show_message("BTC " + BTC, text_colour=[255, 215, 0])
+    sense.show_message("Steem " + Steem, text_colour=[7, 24, 138])
+    sense.show_message("SBD " + '{0:f}'.format(SBD_USD), text_colour=[4, 112, 22])
+  except:
+    sense.show_message("failed to retrieve feed")
