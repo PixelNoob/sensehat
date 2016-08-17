683df6f73ee29ab90b94a163601e32b69a71593b
PixelNoob <jesuschitty7@hotmail.com>
2016-08-17 12:44:47 -0300

    create steem_price
    Steem and SBD price added to price feed

---
+++ steem_price.py
@@ -0,0 +1,16 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+from urllib.request import urlopen
+import json
+
+url = urlopen('https://poloniex.com/public?command=returnTicker').read()
+result = json.loads(url)
+
+SBD = result['BTC_SBD']['last']
+BTC = result['USDT_BTC']['last']
+Steem = result['BTC_STEEM']['last']
+SBD_USD = float(SBD) * float(BTC)
+
+sense.show_message("BTC " + BTC, text_colour=[255, 215, 0])
+sense.show_message("Steem " + Steem, text_colour=[7, 24, 138])
+sense.show_message("SBD " + '{0:f}'.format(SBD_USD), text_colour=[4, 112, 22])
